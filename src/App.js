58d6e476f58a0544cc33f2e683d5ef9e79f7d894
JonathanRicardo <jonricfel@gmail.com>
2020-10-04 01:57:00 -0300

    Setting up basic routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
+
+const App = () => (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Learn STEM and get inspired by your own scientific heroes!</p>
-      </header>
+      <div className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" />
+        </Link>
+        <div>
+          <Link className="App-link" to="/about">About</Link>
+          <Link className="App-link" to="/demo">Demo</Link>
+        </div>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/demo">
+          <Demo />
+        </Route>
+      </Switch>
     </div>
+  </Router>
+);
+
+const Home = () => {
+  return (
+    <h2>Home</h2>
   );
 }
+
+const About = () => {
+  return (
+    <h2>About</h2>
+  );
+}
+
+const Demo = () => {
+  return (
+    <h2>Demo</h2>
+  );
+}
+
 
 export default App;
