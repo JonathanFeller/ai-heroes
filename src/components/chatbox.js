74765cf5dbc6d820e3db8ea2c518b22657526b54
JonathanRicardo <jonricfel@gmail.com>
2020-10-05 01:04:55 -0300

    Now adds messages as times goes by in sync with video

--- src/components/chatbox.js
+++ src/components/chatbox.js
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './chatbox.css';
 
 const ChatBox = () => {
+
+  useEffect(() => {
+    const firstMessage = setTimeout(() => {
+      setMessages(m => [...m,
+      { text: "I am a German born theoretical physicist.", type: 'answer' },
+      ]);
+    }, 4500);
+    const secondMessage = setTimeout(() => {
+      setMessages(m => [...m,
+      { text: "Maybe mostly known because of my equation E=mc^2.", type: 'answer' },
+      ])
+    }, 8000);
+    const thirdMessage = setTimeout(() => {
+      setMessages(m => [...m,
+      { text: "What would you like to know more about?", type: 'answer' }
+      ])
+    }, 13000);
+    return () => {
+      clearTimeout(firstMessage);
+      clearTimeout(secondMessage);
+      clearTimeout(thirdMessage);
+    }
+  }, []);
+
   const [message, setMessage] = useState('');
 
   const [messages, setMessages] = useState([
     { text: "Who are you?", type: 'question' },
     { text: "Hi! My name is Albert Einstein.", type: 'answer' },
-    { text: "I am a German born theoretical physicist.", type: 'answer' },
-    { text: "Maybe mostly known because of my equation E=mc^2.", type: 'answer' },
-    { text: "What would you like to know more about?", type: 'answer' },
   ]);
 
   const renderChat = () => messages.map((msg, i) =>
