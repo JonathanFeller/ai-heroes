4540f0a413fddad52a67c0feab1979dd56eef43f
JonathanRicardo <jonricfel@gmail.com>
2020-10-04 21:17:53 -0300

    Minor changes to demo

--- src/components/demo.js
+++ src/components/demo.js
@@ -8,7 +8,8 @@ const Demo = () => (
       <div className="blackboard">
         <div className="user-account">
           <iframe title="video" width="100%" height="100%"
-            src="https://www.youtube.com/embed/izDxpfOIF6M?rel=0&amp;autoplay=1&mute=1&loop=1" frameBorder="0"
+            src="https://www.youtube.com/embed/izDxpfOIF6M?rel=0&amp;autoplay=1&mute=0&loop=1" frameBorder="0"
+            autoplay
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen></iframe>
         </div>
@@ -18,7 +19,10 @@ const Demo = () => (
     </section>
     <section className="section-video">
       <div className="mascota">
-        <iframe title="blackboard" width="100%" height="100%" src="https://images-assets.nasa.gov/video/GSFC_20181002_SMBH_m13043_Simulation/GSFC_20181002_SMBH_m13043_Simulation~orig.mp4?rel=0&amp;autoplay=1&mute=1&loop=1" frameborder="0" autoplay muted allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe title="blackboard" width="100%" height="100%"
+          src="https://images-assets.nasa.gov/video/GSFC_20181002_SMBH_m13043_Simulation/GSFC_20181002_SMBH_m13043_Simulation~orig.mp4?rel=0&amp;autoplay=1&mute=1&loop=1"
+          frameborder="0" autoplay
+          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
     </section>
   </div>
